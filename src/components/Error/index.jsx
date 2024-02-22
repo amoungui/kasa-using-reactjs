863ed5b059d3725ff0aa6727f52aa5b1e1b14fe3
amoungui <samoungui@gmail.com>
2024-02-22 13:19:25 +0100

    add react-router-dom to 404 page

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom'
+
 function Error() {
     return (
         <main className="noexist-page">
             <span className="noexist-page-info">404</span>
             <p className="noexist-page-msg">Oups! La page que vous demandez n'existe pas.</p>
-            <a href="index.html" className="noexist-page-link">Retourner sur la page d’accueil</a>
+            <Link to="/" className="noexist-page-link">Retourner sur la page d’accueil</Link>
         </main>
     )
 }
