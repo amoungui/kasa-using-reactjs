e8924d40260eb296c1dc8513ecec7a42df44db14
amoungui <samoungui@gmail.com>
2024-02-22 17:08:52 +0100

    review card link

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,19 +4,20 @@ import { Link } from 'react-router-dom'
 function Card({ label, title, url, id }) {
     const link = `/article/${id}`; 
     console.log('id: ', id)
-    return (        
-        <article className="card">
-            <Link to={link}>
-                <img src={url} alt={label} height={80} width={80} />
-            </Link>                                
-            <div className="card-content">
-                <div className="card-txt">
-                    <Link to={link} >
-                        <p className="card-title">{title}</p>
-                    </Link>
+    return (
+        <Link to={link}>
+            <article className="card">            
+                    <img src={url} alt={label} height={80} width={80} />          
+                <div className="card-content">
+                    <div className="card-txt">
+                        <Link to={link} className='card-link'>
+                            <p className="card-title">{title}</p>
+                        </Link>
+                    </div>
                 </div>
-            </div>
-        </article>
+            </article>
+        </Link>        
+
     )
 }
 
