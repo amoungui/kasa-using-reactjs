e844712f666a26d41de59a1c4fa8f3f6cbc74a5c
amoungui <samoungui@gmail.com>
2024-02-22 14:00:30 +0100

    get an object by id using fetch on Article.jsx page

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,14 +1,25 @@
-import { useParams } from 'react-router-dom'
- 
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
 function Article() {
-    const { articleNumber } = useParams()
- 
+    const { articleNumber } = useParams();
+    const [article, setArticle] = useState(null);
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/logements/${articleNumber}`)
+            .then(response => response.json())
+            .then(data => setArticle(data))
+            .catch(error => console.error('Error:', error));
+    }, [articleNumber]);
+
+    if (!article) return 'Loading...';
+
     return (
         <div>
             <h1>Article 🧮</h1>
-            <h2>Article {articleNumber}</h2>
+            <h2>{article.title}</h2>
         </div>
-    )
+    );
 }
 
-export default Article
+export default Article;
