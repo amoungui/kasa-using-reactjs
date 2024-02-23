9bad6dd70527ea4b402193aae048dea02c7b0868
amoungui <samoungui@gmail.com>
2024-02-23 09:30:06 +0100

    manage active link on nav menu using useEffet on Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,21 @@
 import { Link } from 'react-router-dom'
- 
+import { useEffect } from 'react'
+
 function Header() {
+    useEffect(()=>{
+        let links = Array.from(document.getElementsByClassName('btn-link'));
+        links.forEach(link => {
+            link.addEventListener('click', (e) => {
+                // Suppression de la classe active de tous les liens
+                links.forEach((link) => {
+                    link.classList.remove("active");
+                });
+                // Ajout de la classe active au lien cliqué
+                e.currentTarget.classList.add("active");
+            });
+        });
+    }, []);
+    
 
     return (
         <header>
@@ -8,7 +23,7 @@ function Header() {
                 <img src={process.env.PUBLIC_URL + '/assets/img/logo-header.jpg'} className="header-logo" alt="Logo kasa"/>
             </Link>
             <nav className="menu-nav">
-                <Link to="/" className="btn-link btn-home  active">Accueil</Link>
+                <Link to="/" className="btn-link btn-home active">Accueil</Link>
                 <Link to="/about/" className="btn-link btn-about">A propos</Link>
             </nav>
         </header>
